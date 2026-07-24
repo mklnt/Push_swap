41970d50b8d4a285e937b335bc4b5a27a96fa542
mklnt <lentini.michelemaria@gmail.com>
2026-07-24 02:05:31 +0200

    complex algorithm done

---
+++ complex_radix_lsd.c
@@ -0,0 +1,52 @@
+void	convert_array(int *arr_a)
+{
+	int	i;
+
+	i = 0;
+	while(arr[i] != '\0')
+	{	
+		arr_a[1] = convert_binary(arr[1]); // we need a simple function to convert a decimal into binary
+		i ++;
+	}	
+}
+
+void	lsd(int *arr_a, int **arr_b)
+{
+	int	exp;
+	int	max;
+
+	exp = 1;
+	convert_array(arr_a);
+	max = get_max(arr_a);
+	while (max / exp != 0)
+	{
+		while (arr_a)
+		{
+			if (arr_a[0] / exp = 0)
+				pb;
+			else
+			{
+				pb;
+				rb;
+			}
+		}
+		while (arr_b)
+		{
+			if (arr_b[0] / exp = 0)
+				pa;
+			else
+			{
+				pa;
+				ra;
+			}
+		}
+		exp *= 10;
+	}
+	if (!arr_a)
+		while (arr_b)
+		{
+			pa;
+			ra;
+		}
+	revert_binary(arr_a);//and viceversa
+}
